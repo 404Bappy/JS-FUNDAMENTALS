12038a3619832f8bf7cc8b55a56ec3b2a82ab091
Bappy71 <19103076@iubat.edu>
2022-07-25 02:59:49 -0700

    first commit

---
+++ Array.js
@@ -0,0 +1,14 @@
+var friendsAge = [12, 18, 19];
+
+var KhaboVortaFee = [50, 60, 70, 80];
+
+var sports = ['football', 'cricket', 'kabadi']
+
+var oddNumbers = [1, 3, 5, 7, 9, 11];
+
+var vowels = ['a', 'e', 'i', 'o', 'u'];
+
+console.log(vowels);
+
+// Array length//
+console.log(sports.length);
