756528adbd8e4a53e4e6c83fb1814f9d5a6e95ca
Bappy71 <19103076@iubat.edu>
2022-07-25 03:55:40 -0700

    first commit

---
+++ Array-index.js
@@ -0,0 +1,16 @@
+var bookPages = ['messi', 'ronaldo', 'cruif', 'xavi', 'ineasta'];
+var ronaldoIndex = bookPages.indexOf('ronaldo');
+console.log(ronaldoIndex);
+
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+var index = numbers.indexOf(5);
+console.log(index);
+
+var thirdIndex = numbers[3];
+console.log(thirdIndex);
+
+// change value in array by using index!
+
+numbers[2] = 9;
+console.log(numbers)
+
