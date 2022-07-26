38212c80670ce67d6c9f4411733363a1c5836347
Bappy71 <19103076@iubat.edu>
2022-07-26 03:43:58 -0700

    first commit

---
+++ Conditional/Multiple-condition.js
@@ -0,0 +1,22 @@
+var gotJob = true;
+
+var moneySaved = 180000;
+
+var haveFlat = true;
+
+if(gotJob == true && moneySaved > 200000 && haveFlat == true){
+    console.log('Cholo biye kori');
+}
+
+else{
+    console.log('kopale biye nei');
+}
+
+
+if(gotJob == true || moneySaved > 200000){
+    console.log('Cholo biye kori');
+}
+
+else{
+    console.log('kopale biye nei');
+}
