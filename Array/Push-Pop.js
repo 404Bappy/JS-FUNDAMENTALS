6ffb37d14265dc57c664d4a97445bf1fdaf89d4d
Bappy71 <19103076@iubat.edu>
2022-07-26 02:40:40 -0700

    first commit

---
+++ Array/Push-Pop.js
@@ -0,0 +1,25 @@
+//Add elements to an array!! (PUSH)
+
+var busSits = ['bappy', 'kobir', 'rohim', 'taklu', 'paglu'];
+busSits.push('khalid');
+busSits.push('rahat');
+busSits.push('nioy');
+
+console.log(busSits);
+
+var boysAge = [1, 4, 9, 10, 15, 20];
+boysAge.push(15);
+console.log(boysAge);
+
+//Remove element from an Array!! (POP)
+
+boysAge.pop();
+boysAge.pop();
+boysAge.pop();
+console.log(boysAge);
+
+var lastItem = boysAge.pop();
+console.log(lastItem);
+
+//Javascript remove item from the beginning of the array
+//Javascript add item in the beginning from array.
