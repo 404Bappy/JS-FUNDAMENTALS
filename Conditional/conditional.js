4d933a8c0fe88cab77812338cfbfb3c1e8f9162b
Bappy71 <19103076@iubat.edu>
2022-07-26 03:30:36 -0700

    first commit

---
+++ Conditional/conditional.js
@@ -0,0 +1,44 @@
+var isFoodReady = true;
+//If food is ready i will eat...!!
+//if (condition) {
+    
+//}
+
+if(isFoodReady == true) {
+    console.log('ammu i will eat now')
+};
+
+var iphonePrice = 70000;
+var myBudget = 90000;
+
+if(iphonePrice < myBudget); {
+    console.log("futani marbo");
+
+}
+
+
+if(iphonePrice > myBudget); {
+    console.log("i bougt my walton phone");
+}
+
+//       If Else Condition      //
+
+//if(condition){
+
+    //
+//}
+
+//else{
+
+    //
+//}
+
+var ballPrice = 90;
+var myMoney = 70;
+
+if(ballPrice < myMoney){
+    console.log('khela hobe....!!')
+}
+else{
+    console.log('khela hbe nah...!!')
+}
