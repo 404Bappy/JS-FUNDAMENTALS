ec3cbe18b3fd99582e0e2b98da73c93bcdebe7b9
Bappy71 <19103076@iubat.edu>
2022-07-26 02:57:35 -0700

    first commit

---
+++ Array/comparison.js
@@ -0,0 +1,15 @@
+console.log(5 > 6 );
+console.log(5 > 3);
+console.log(5 == 3);
+console.log(5 == 5);
+console.log(5 != 6);
+
+var myLove = 99;
+var hisLove = 98;
+console.log(myLove > hisLove);
+console.log(myLove == hisLove);
+console.log(myLove < hisLove);
+console.log(myLove != hisLove);
+
+console.log(studyGood && goodPerson);
+console.log(isBanker || goodResult);
