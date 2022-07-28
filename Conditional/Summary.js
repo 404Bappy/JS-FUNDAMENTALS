4d12fc984e1854d4b5e9e4e178a3105992b99ed5
Bappy71 <19103076@iubat.edu>
2022-07-28 04:27:46 -0700

    first commit

---
+++ Conditional/Summary.js
@@ -0,0 +1,34 @@
+var tableLength =12;
+var tourDestinations =['coxbzr', 'vutan', 'paris', 'nepal'];
+ 
+tourDestinations.indexOf('paris');
+
+var forthDestinatios[1] = 'uganda';
+
+tourDestinations.push('london');
+tourDestinations.pop();
+
+if(tourDestinations[1] == 'nepal'){
+    console.log('Shera shera shera');
+}
+else if (tourDestinations[1] == 'china'){
+    console.log('chaina tor kache jai nah');
+}
+else if(tourDestinations.length == 4){
+    console.log('aro ghurbo ami');
+}
+else{
+    console.log('vallage nah ghurbo na');
+}
+
+
+var  ballPrice = 80;
+var  myBudget = 90;
+
+if(ballPrice < myBudget){
+    console.log('ami ball khelbo');
+}
+
+if(ballPrice > myBudget){
+    console.log( 'khela bondho');
+}
