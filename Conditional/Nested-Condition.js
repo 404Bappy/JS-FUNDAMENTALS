42b82f71ab0ff50397f1cb037269cc6fd74cbb68
Bappy71 <19103076@iubat.edu>
2022-07-27 23:56:36 -0700

    first commit

--- Conditional/Nested-Condition.js
+++ Conditional/Nested-Condition.js
@@ -1,7 +1,8 @@
 var danishPrice = 5;
 var butterBreadPrice = 25;
 var toastBiscuitPrice = 10;
-var myBudget = 100;SSMNN 
+var myBudget = 100;
+var packedWell = false;
 
 if (danishPrice < myBudget) {
     console.log("danish kyeye danish jabo");
@@ -10,14 +11,19 @@ if (danishPrice < myBudget) {
 else if (butterBreadPrice < myBudget) {
     console.log('Butter bon diye cha khabo');
 }
-else if(toastBiscuitPrice < myBudget){
+else if (toastBiscuitPrice < myBudget) {
     console.log('Tosat Bisceatr khamu');
 }
 
-else{
+else {
     console.log('Batash diye cha khabo');
 }
 
-if(danishPrice < myBudget){
-   if()
+if (danishPrice < myBudget) {
+    if (packeedWll == true) {
+        console.log('danish khabo')
+    }
+    else {
+        console.log('danish khamu na cz masi khai felse')
+    }
 }
