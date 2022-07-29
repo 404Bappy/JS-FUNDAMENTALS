e5db407b582326707b888964c5fe6dab82e1808e
Bappy71 <19103076@iubat.edu>
2022-07-29 02:06:43 -0700

    first commit

---
+++ Recap-Loop/For-Loop.js
@@ -0,0 +1,23 @@
+for(var roastGiven = 0; roastGiven < 7; roastGiven++){
+    console.log('Roast den,please!!');
+    console.log(roastGiven);
+}
+
+for( var i = 0; i < 7; i++){
+    console.log(i);
+}
+
+//Odd Numbers//                  //i+=2;
+                                 //i = i +2
+
+
+for(var i = 1; i<=20; i+=2){
+    console.log(i);
+}
+
+
+// Even Number//
+
+for(var i = 0; i<= 60; i+=2){
+    console.log(i);
+}
