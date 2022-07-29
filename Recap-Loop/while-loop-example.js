d9c6bed381ed3d693fea07c790d4da74c7a5bb0c
Bappy71 <19103076@iubat.edu>
2022-07-29 01:39:31 -0700

    first commit

---
+++ Recap-Loop/while-loop-example.js
@@ -0,0 +1,20 @@
+var number = 0; //loop variable
+
+while(number <= 10){
+    console.log(number);
+    number++;
+}
+
+//Odd NUmbers till 20//
+while(number <= 20){
+    console.log(number);
+    number = number + 2;
+}
+
+//Even Numbers//
+
+number = 0;
+while(number <= 20){
+    console.log(number);
+    number = number + 2;
+}
