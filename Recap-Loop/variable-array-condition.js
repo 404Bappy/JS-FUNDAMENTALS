0e59482799fd900be0bf1e28483b0c01d66402cb
Bappy71 <19103076@iubat.edu>
2022-07-29 01:06:14 -0700

    first commit

---
+++ Recap-Loop/variable-array-condition.js
@@ -0,0 +1,24 @@
+var bottleColor = 'yellow';
+var waterQuantity = 1 ;
+var isFull = false;
+
+// Array //
+var items = ['ball', 'bat', 'stump', 'gun'];
+items.indexOf('bat');
+items.indexOf('football'); //-1
+items.push('jersey');
+items.push('gard');
+
+items,pop();
+
+//Conditionals //
+if(items.length >= 4){
+    crossOriginIsolated.log('you are in wrong way');
+
+}
+else if(items.length == 4){
+    console.log('you are in correct way');
+}
+else{
+    console.log('WOW! Great Work');
+}
