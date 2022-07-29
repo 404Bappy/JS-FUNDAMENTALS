8e5bd3e078cb392a5f933c483cbc385fc97bc0c0
Bappy71 <19103076@iubat.edu>
2022-07-29 02:34:41 -0700

    first commit

---
+++ Recap-Loop/Array-loop.js
@@ -0,0 +1,13 @@
+var numbers = [40, 50, 60, 70, 80, 90, 100, 70, 80];
+
+for(var i = 0; i < 7; i++){
+    var element = numbers[i];
+    console.log(element);
+}
+
+var items = ['amm', 'jam', 'lichu', 'kathal', 'kodhu', 'komola', 'pepe'];
+
+for(var i = 0; i < items.length; i++){
+    var item = items[i];
+    console.log(item);
+}
