4e43b0d70ab79720078b681db59baad2220dcb06
Bappy71 <19103076@iubat.edu>
2022-07-30 03:10:58 -0700

    first commit

---
+++ Explore-Function-Object/Parameter.js
@@ -0,0 +1,10 @@
+function bringSingara(taka){
+   console.log('Singara Price', taka);
+   console.log('mama 10 ta singara den to..!!');
+   var singaraPrice = 10;
+   var singaraQuantity = taka / singaraPrice ;
+   return singaraQuantity;
+}
+var money = 90;
+var singara = bringSingara(money);
+console.log('ai lon mama', singara);
