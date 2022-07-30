ced7bbf77005f272202305d62a258bad9db7b04c
Bappy71 <19103076@iubat.edu>
2022-07-30 02:00:17 -0700

    first commit

---
+++ Explore-Function-Object/Function.js
@@ -0,0 +1,20 @@
+
+// Function Decleration //
+
+function turnonFan(){
+    console.log('walk towards the switch');
+    console.log('press the switch');
+}
+
+//Call Functio//
+
+turnonFan();
+
+console.log('eat breakfast');
+console.log('Drink tea');
+console.log('take shower');
+console.log('Checking Notifications');
+
+turnonFan();
+turnonFan();
+turnonFan();
