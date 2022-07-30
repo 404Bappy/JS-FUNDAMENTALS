6d593a86dadfd84c649488442930090e76f8d5a2
Bappy71 <19103076@iubat.edu>
2022-07-30 01:42:22 -0700

    first commit

---
+++ Explore-Function-Object/Recap.js
@@ -0,0 +1,34 @@
+var favotiteBook = 'Bela furabar age'
+
+//Array//
+
+var bookList = ['abcd', 'efgh', 'ijkl', 'mnop', 'qrst', 'uvwx'];
+
+var efghIndex = bookList.indexOf('mnop');
+bookList.push('ki diye ki');
+
+bookList.pop();
+
+//Conditionals//
+
+if(bookList[1]== 'efgh'){
+    console.log('yes Correct');
+}
+else{
+    console.log('Incorrect Ans ');
+}
+
+// while Loop //
+ 
+var i = 0; //loop variable//
+while(i < 15){
+    console.log(i);
+    console.log('looping..' ,'looping.');
+    i++;
+}
+
+//For Loop //
+var i = 20;
+for(var i = 0; i<15 ; i++){
+    console.log(i);
+}
