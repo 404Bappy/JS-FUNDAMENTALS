f1da2f3aab0000f27a7280b80590312822c8bf87
Bappy71 <19103076@iubat.edu>
2022-08-01 23:57:11 -0700

    first commit

--- Explore-Function-Object/Multiple-parameter.js
+++ Explore-Function-Object/Multiple-parameter.js
@@ -1,5 +1,12 @@
 function addTwoNumbers(number1 , number2){
     console.log(number1 , number2);
+    var total = number1 + number2;
+    return total;
 }
 
-addTwoNumbers(21 , 70);
+var firstNumber = 35;
+var secondNumber = 47;
+var result = addTwoNumbers(firstNumber , secondNumber);
+console.log('result value: ', result);
+
+function
