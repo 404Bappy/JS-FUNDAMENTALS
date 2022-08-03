9161d30a9ef4fea7984f83f4337d8bea721fa49b
Bappy71 <19103076@iubat.edu>
2022-08-03 02:16:57 -0700

    first commit

---
+++ Explore-Function-Object/Object.js
@@ -0,0 +1,21 @@
+var student1 = {
+    id: 115,
+    name: 'bappy',
+    marks: 70,
+    mobile: 12345677
+};
+
+var mobile = {
+    color: 'Black',
+    price: 30000,
+    ram  : 3,
+    rom : 128,
+};
+
+var Book ={
+    title: 'toom',
+    authot: 'mama jan',
+    price : 300,
+    pages : 500,
+    publisher : 'kaktarua'
+}
