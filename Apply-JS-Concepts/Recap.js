5e8c5353e71880e1095a52cf00dfd83e783195e6
Bappy71 <19103076@iubat.edu>
2022-08-06 02:19:17 -0700

    first commit

---
+++ Apply-JS-Concepts/Recap.js
@@ -0,0 +1,15 @@
+var time = '10:15 Am';
+var bookprice =150;
+var isWhiteColor = false;
+
+//Arrary 
+var brothers = ['rofiq', 'jobbar', 'salam', 'kalam'];
+
+                //to know elemrnt//
+ var elementCount =    brothers.length;
+
+//  to know index of element//
+ var jobbarIndex =  brothers.indexOf('jobbar');
+
+ brothers.push('bappy');
+ console.log(brothers);
