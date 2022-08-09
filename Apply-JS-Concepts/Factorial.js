da72f14884727ba937d191604c4ed97ff54b8ba7
Bappy71 <19103076@iubat.edu>
2022-08-08 23:54:41 -0700

    first commit

--- Apply-JS-Concepts/Factorial.js
+++ Apply-JS-Concepts/Factorial.js
@@ -2,3 +2,10 @@
 // 4! = 4 * 3 * 2 * 1
 // 5! = 5 * 4 * 3 * 2 * 1
 // 6! = 6 * 5 * 4 * 3 * 2 * 1
+
+for(var i = 1; i < 7; i++){
+    console.log(i);
+    var factorial = 1;
+    factorial = factorial * i;
+}
+
