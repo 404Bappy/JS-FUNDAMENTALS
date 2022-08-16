ef36d4dcb3e0769df1483dd4294d8fb77ffea93e
Bappy71 <19103076@iubat.edu>
2022-08-16 19:06:14 +0600

    first commit

---
+++ JS-simple-problems/max.js
@@ -0,0 +1,18 @@
+const business = 4950;
+const minister = 750;
+const army = 600;
+Compare with three
+if(business > minister && business > army){
+    console.log("business is bigger");
+}
+else if(minister > business && minister > army){
+    console.log("minister is bigger ");
+}
+else{
+    console.log("army is bigger");
+}
+
+var max= Math.max(business , minister , army);
+console.log('largest is :', max);
+
+
