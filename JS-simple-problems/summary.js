b2df15c5faac3a893844341caba084c7350a411f
Bappy71 <19103076@iubat.edu>
2022-08-17 02:53:48 +0600

    first commit

---
+++ JS-simple-problems/summary.js
@@ -0,0 +1,20 @@
+const number = 1.2356;
+
+//Math.floor
+Math.ceil
+Math.round
+
+const random = Math.random()
+const between10 = random * 6;
+const rounded = Math.round(between10);
+console.log(rounded);
+
+
+for( let i =0; i < 5; i++){
+    const random =Math.random() *100;
+    const selected = Math.round(random);
+    if(selected.indexOf(selected) == -1){
+        selected.push(picked);
+    }
+    console.log(selected);
+}
