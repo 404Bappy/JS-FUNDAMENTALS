f2f7a50053514c5748621288174bfa683b254a6c
Bappy71 <19103076@iubat.edu>
2022-08-16 18:21:24 +0600

    first commit

--- JS-simple-problems/math.js
+++ JS-simple-problems/math.js
@@ -1,20 +1,20 @@
-// const myNumber = -5;
-// const output = Math.abs(myNumber);
-// console.log(output);
+const myNumber = -5;
+const output = Math.abs(myNumber);
+console.log(output);
 
 
-// const myNumber = 3.6666668 ;
-// const output = Math.ceil(myNumber);
-// console.log(output);
+const myNumber = 3.6666668 ;
+const output = Math.ceil(myNumber);
+console.log(output);
 
-// const mynumber = 5.4455 ; 
-// const output = Math.floor(mynumber);
-// console.log(output);
+const mynumber = 5.4455 ; 
+const output = Math.floor(mynumber);
+console.log(output);
 
 
-// const mynumber = 5.7777 ; 
-// const output = Math.round(mynumber);
-// console.log(output);
+const mynumber = 5.7777 ; 
+const output = Math.round(mynumber);
+console.log(output);
 
 const output = Math.random() *10;
 console.log(output);
