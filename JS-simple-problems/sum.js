07ef766bbe0e3bff69440286111670ef7243cc70
Bappy71 <19103076@iubat.edu>
2022-08-17 00:46:28 +0600

    first commit

--- JS-simple-problems/sum.js
+++ JS-simple-problems/sum.js
@@ -1,11 +1,11 @@
 const number = [11 , 33 ,66, 77, 99, 67, 90, 87];
 
-// let sum = 0;
-// for(i = 0; i < number.length; i++){
-//     const element = number[i];
-//     sum = sum + element;
-//     console.log(sum);
-// }
+let sum = 0;
+for(i = 0; i < number.length; i++){
+    const element = number[i];
+    sum = sum + element;
+    console.log(sum);
+}
 
 function arrayTotal (number){
     let sum = 0;
