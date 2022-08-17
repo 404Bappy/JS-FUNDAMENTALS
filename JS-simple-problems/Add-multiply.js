9b27e9866fdfc3eff311f4a43e3a63786550c529
Bappy71 <19103076@iubat.edu>
2022-08-18 04:31:21 +0600

    first commit

---
+++ JS-simple-problems/Add-multiply.js
@@ -0,0 +1,21 @@
+function add(num1 , num2){
+    const sum = num1 + num2;
+    return sum;
+}
+const firstTotal = add(70 ,80);
+console.log(firstTotal);
+const secondTotal= add(80 , 90);
+console.log(secondTotal);
+
+
+// Multiply //
+
+function multiply(number1 , number2){
+    const multiplication = number1 * number2 ;
+    return multiplication ;
+}
+
+const multiplication1 = multiply(100 , 9);
+console.log(multiplication1);
+const multiplication2 = multiply(100 , 5);
+console.log(multiplication2);
