a153e5b60de04487d904b6bda7a807670e123c55
Bappy71 <19103076@iubat.edu>
2022-08-20 12:03:20 +0600

    first commit

--- JS-DOM/DOM-Event/EVENT.js
+++ JS-DOM/DOM-Event/EVENT.js
@@ -1,16 +1,33 @@
 
-        function makeRed() {
-            document.body.style.backgroundColor = 'red';
-        }
+function makeRed() {
+    document.body.style.backgroundColor = 'red';
+}
 
-        const blueButton = document.getElementById('my-blueButtonn');
-        blueButton.onclick = makeBlue;
+const blueButton = document.getElementById('my-blueButtonn');
 
-        function makeBlue() {
-            document.body.style.backgroundColor = 'blue';
-        }
+//Just set the name of the function
+
+blueButton.onclick = makeBlue;
+
+function makeBlue() {
+    document.body.style.backgroundColor = 'blue';
+}
+
+//Handle Event Using Anonymous Function
+
+const greenButton = document.getElementById('my-greenButton');
+greenButton.onclick = function () {
+    document.body.style.backgroundColor = 'Green';
+}
 
 
+//Handle by using add eventlistener
 
+const GreenYellow = document.getElementById('my-greenYellowButton');
+GreenYellow.addEventListener('click' , makeGreenYellow);
+
+function makeGreenYellow(){
+    document.body.style.backgroundColor = 'greenYellow';
+}
 
 
