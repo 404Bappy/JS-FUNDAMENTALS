84323528b42f543ba6b335552a873d20f474d8e5
Bappy71 <19103076@iubat.edu>
2022-08-20 11:11:32 +0600

    first commit

--- JS-DOM/DOM-Event/EVENT.js
+++ JS-DOM/DOM-Event/EVENT.js
@@ -1,4 +1,15 @@
 
+        function makeRed() {
+            document.body.style.backgroundColor = 'red';
+        }
+
+        const blueButton = document.getElementById('my-blueButtonn');
+        blueButton.onclick = makeBlue;
+
+        function makeBlue() {
+            document.body.style.backgroundColor = 'blue';
+        }
+
 
 
 
